3735eb9104d57e38689f34b2961715fab1ecdf54
khangzxrr <khangzxrr@gmail.com>
2023-05-03 01:19:01 +0700

    add get order function

---
+++ lib/providers/order.dart
@@ -0,0 +1,30 @@
+
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:fastship_shipper/libs/authorizeClient.dart';
+import 'package:fastship_shipper/models/order.dart';
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+
+class OrderProvider extends ChangeNotifier {
+  late Iterable<OrderShippingModel> orderShippings;
+
+
+  void getOrderShippings() async {
+    final http = GetIt.instance<AuthorizeClient>();
+    final orderEndpoint = http.generateApi('/shipper/orders');
+
+    final response = await http.get(orderEndpoint);
+
+    if (response.statusCode == 200){
+      final jsonObject = json.decode(response.body);
+
+      orderShippings = (jsonObject['shipperOrderRecords'] as List).map((json) => OrderShippingModel.fromJson(json));
+
+      print(orderShippings);
+    }
+
+    //log(response.body.toString());
+  }
+}
