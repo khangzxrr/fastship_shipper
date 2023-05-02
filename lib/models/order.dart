777a81665e8ade84ca2bb3d872ed80d94f971a58
khangzxrr <khangzxrr@gmail.com>
2023-05-03 01:18:41 +0700

    add factory and many properties

--- lib/models/order.dart
+++ lib/models/order.dart
@@ -1,10 +1,31 @@
-class Order {
+class OrderShippingModel {
+
+  int orderShippingId;
+
   String customerName;
   String customerAddress;
-  String customerPhonenumber;
+  String customerPhoneNumber;
+  String orderShipingStatus;
 
   double remainCost;
 
-  Order(this.customerName, this.customerAddress, this.customerPhonenumber,
-      this.remainCost);
+  OrderShippingModel(this.orderShippingId, this.customerName, this.customerAddress,this.customerPhoneNumber, this.orderShipingStatus, this.remainCost);
+
+  @override
+  String toString() {
+    return '$orderShippingId $customerName $customerAddress $customerPhoneNumber $orderShipingStatus $remainCost';
+  }
+
+  factory OrderShippingModel.fromJson(Map<String, dynamic> parsedJson) {
+    return OrderShippingModel(
+      parsedJson['orderShippingId'], 
+      parsedJson['customerName'], 
+      parsedJson['customerAddress'],
+      parsedJson['customerPhoneNumber'],
+      parsedJson['orderShipingStatus'],
+      (parsedJson['remainCost'] is int) ? (parsedJson['remainCost'] as int).toDouble() : parsedJson['remainCost']
+      );
+
+
+  }
 }
