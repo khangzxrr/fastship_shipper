4d662992319d4eefd2becc26d5344eb5b16a8392
khangzxrr <khangzxrr@gmail.com>
2023-05-03 01:19:25 +0700

    Finish display real data

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,5 +1,8 @@
+import 'package:fastship_shipper/models/order.dart';
+import 'package:fastship_shipper/providers/order.dart';
 import 'package:fastship_shipper/views/mapToCustomer.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class MyHomePage extends StatefulWidget {
   @override
@@ -33,36 +36,39 @@ class _MyHomePageState extends State<MyHomePage> {
 class CurrentOrderPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      padding: EdgeInsets.all(15),
-      child: Column(
-        children: [
-          Text('Today you have 6 orders'),
-          Order(),
-          Order(),
-          Order(),
-          Order(),
-          Order(),
-          Order(),
-          Order(),
-          Order(),
-        ],
-      ),
-    );
+    return Consumer<OrderProvider>(builder: (context, orderProvider, child) {
+
+      orderProvider.getOrderShippings();
+
+      return SingleChildScrollView(
+        padding: EdgeInsets.all(15),
+        child: Column(
+          children: [
+            Text('Today you have ${orderProvider.orderShippings.length} orders'),
+            for (var orderShipping in orderProvider.orderShippings) Order(orderShippingModel: orderShipping)
+          ],
+        ),
+      );
+    });
   }
 }
 
 class Order extends StatelessWidget {
+
+  OrderShippingModel orderShippingModel;
+
+  Order({required this.orderShippingModel});
+
   @override
   Widget build(BuildContext context) {
     return Card(
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          const ListTile(
-            leading: Icon(Icons.shopping_basket),
-            title: Text('Order 1'),
-            subtitle: Text('Nguoi nhan abc\nDia chi: def\nSo dien thoai: aaa'),
+          ListTile(
+            leading: const Icon(Icons.shopping_basket),
+            title: const Text('Order 1'),
+            subtitle: Text('Người nhận: ${orderShippingModel.customerName}\nĐịa chỉ: ${orderShippingModel.customerAddress}\nSố điện thoại: ${orderShippingModel.customerPhoneNumber}\nTrạng thái: ${orderShippingModel.orderShipingStatus}\nSố tiền còn lại: ${orderShippingModel.remainCost}'),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.end,
