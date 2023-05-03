4c46faf5a0bd0d32ad732aa6d7c63ead2755f7e9
khangzxrr <khangzxrr@gmail.com>
2023-05-04 01:22:06 +0700

    init GoongGeo DI

--- lib/get_it_DI/configDependencies.dart
+++ lib/get_it_DI/configDependencies.dart
@@ -1,15 +1,15 @@
-
 import 'package:fastship_shipper/libs/authorizeClient.dart';
+import 'package:fastship_shipper/services/goong_geocoding.dart';
 import 'package:get_it/get_it.dart';
 
 class ConfigDependencies {
-  
   final getIt = GetIt.instance;
 
   Future config() async {
-
     await AuthorizeClient.init();
 
     getIt.registerFactory(() => AuthorizeClient());
+
+    getIt.registerFactory(() => GoongGeocoding());
   }
 }
