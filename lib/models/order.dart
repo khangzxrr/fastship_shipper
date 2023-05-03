2b4f89c407f069d5e902908caeea26039d521264
khangzxrr <khangzxrr@gmail.com>
2023-05-04 01:22:38 +0700

    add parse status and convert remainCost to double

--- lib/models/order.dart
+++ lib/models/order.dart
@@ -1,15 +1,32 @@
 class OrderShippingModel {
-
   int orderShippingId;
 
   String customerName;
   String customerAddress;
   String customerPhoneNumber;
+
   String orderShipingStatus;
 
   double remainCost;
 
-  OrderShippingModel(this.orderShippingId, this.customerName, this.customerAddress,this.customerPhoneNumber, this.orderShipingStatus, this.remainCost);
+  OrderShippingModel(
+      this.orderShippingId,
+      this.customerName,
+      this.customerAddress,
+      this.customerPhoneNumber,
+      this.orderShipingStatus,
+      this.remainCost);
+
+  String parseOrderShippingStatus() {
+    switch (orderShipingStatus) {
+      case 'inWarehouse':
+        return 'đang trong kho';
+      case 'customerReceived':
+        return 'đã giao thành công';
+      default:
+        return 'Chưa có trạng thái này';
+    }
+  }
 
   @override
   String toString() {
@@ -18,14 +35,13 @@ class OrderShippingModel {
 
   factory OrderShippingModel.fromJson(Map<String, dynamic> parsedJson) {
     return OrderShippingModel(
-      parsedJson['orderShippingId'], 
-      parsedJson['customerName'], 
-      parsedJson['customerAddress'],
-      parsedJson['customerPhoneNumber'],
-      parsedJson['orderShipingStatus'],
-      (parsedJson['remainCost'] is int) ? (parsedJson['remainCost'] as int).toDouble() : parsedJson['remainCost']
-      );
-
-
+        parsedJson['orderShippingId'],
+        parsedJson['customerName'],
+        parsedJson['customerAddress'],
+        parsedJson['customerPhoneNumber'],
+        parsedJson['orderShipingStatus'],
+        (parsedJson['remainCost'] is int)
+            ? (parsedJson['remainCost'] as int).toDouble()
+            : parsedJson['remainCost']);
   }
 }
