d6750a21e2e41cab3c7b12b3b1658f5701da005a
khangzxrr <khangzxrr@gmail.com>
2023-06-02 13:48:53 +0700

    home page now contain produt issue widget

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,5 +1,7 @@
 import 'package:fastship_shipper/providers/order.dart';
+import 'package:fastship_shipper/providers/product_issue_provider.dart';
 import 'package:fastship_shipper/views/order.dart';
+import 'package:fastship_shipper/views/product_issue_view.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
@@ -10,7 +12,19 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  static List<Widget> pages = <Widget>[
+    OrderListWidget(),
+    ProductIssueListWidget(),
+    Icon(Icons.cottage_rounded)
+  ];
   var selectedIndex = 0;
+
+  void onTap(int index) {
+    setState(() {
+      print(index);
+      selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,11 +35,15 @@ class _MyHomePageState extends State<MyHomePage> {
         appBar: AppBar(
           title: const Text('Fastship shipper'),
         ),
-        body: OrderListWidget(),
+        body: pages[selectedIndex],
         bottomNavigationBar: BottomNavigationBar(
+          currentIndex: selectedIndex,
+          onTap: onTap,
           items: const <BottomNavigationBarItem>[
             BottomNavigationBarItem(
-                icon: Icon(Icons.local_shipping), label: 'Current order'),
+                icon: Icon(Icons.local_shipping), label: 'Order shippings'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.local_shipping), label: 'Product issues'),
             BottomNavigationBarItem(
                 icon: Icon(Icons.settings), label: 'Profile')
           ],
@@ -35,9 +53,68 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
+class ProductIssueListWidget extends StatelessWidget {
+  late RefreshController refreshController;
+
+  ProductIssueListWidget({super.key});
+
+  late BuildContext context;
+
+//handle fail refresh case
+  void onRefreshing() async {
+    final productIssueProvider =
+        Provider.of<ProductIssueProvider>(context, listen: false);
+
+    await productIssueProvider.getProductIssue();
+
+    print('complete');
+
+    refreshController.refreshCompleted();
+  }
+
+  void onLoading() async {
+    final productIssueProvider =
+        Provider.of<ProductIssueProvider>(context, listen: false);
+
+    await productIssueProvider.getProductIssue();
+
+    print('complete');
+    refreshController.loadComplete();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    this.context = context;
+    refreshController = RefreshController(initialRefresh: true);
+
+    return SmartRefresher(
+        enablePullDown: true,
+        enablePullUp: false,
+        controller: refreshController,
+        onRefresh: onRefreshing,
+        onLoading: onLoading,
+        child: Consumer<ProductIssueProvider>(
+            builder: (context, productIssueProvider, child) {
+          return SingleChildScrollView(
+            padding: const EdgeInsets.all(15),
+            child: Column(
+              children: [
+                Text(
+                    'Today you have ${productIssueProvider.productIssues.length} product to ship'),
+                for (var productIssueModel
+                    in productIssueProvider.productIssues)
+                  ProductIssueWidget(
+                      productIssueModel: productIssueModel,
+                      renderShippingButton: true)
+              ],
+            ),
+          );
+        }));
+  }
+}
+
 class OrderListWidget extends StatelessWidget {
-  RefreshController refreshController =
-      RefreshController(initialRefresh: false);
+  late RefreshController refreshController;
 
   OrderListWidget({super.key});
 
@@ -66,6 +143,8 @@ class OrderListWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     this.context = context;
+
+    refreshController = RefreshController(initialRefresh: true);
 
     return SmartRefresher(
         enablePullDown: true,
