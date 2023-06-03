e8c48873df027d77e6754666b1d243ee2ee09c8f
khangzxrr <khangzxrr@gmail.com>
2023-06-04 01:58:03 +0700

    request permission

--- lib/views/map_to_customer_for_product_issue.dart
+++ lib/views/map_to_customer_for_product_issue.dart
@@ -144,6 +144,21 @@ class MapUiBodyState extends State<MapUiBody> {
   }
 
   void markDirectionToCustomer() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
     var devicePosition = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.best);
 
